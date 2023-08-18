1e63c5ac14652fc96fea1a6766490395f3c629b1
Nick Santamaria <nicholas.santamaria@dpc.vic.gov.au>
2023-08-18 11:26:35 +1000

    Added default aws region.

--- internal/helpers/aws.go
+++ internal/helpers/aws.go
@@ -7,9 +7,16 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/kms"
 )
 
+const (
+	AwsDefaultRegion string = "ap-southeast-2"
+)
+
 // Return a KMS client with credentials loaded.
 func AwsKmsClient() *kms.Client {
 	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if cfg.Region == "" {
+		cfg.Region = AwsDefaultRegion
+	}
 	if err != nil {
 		panic("configuration error, " + err.Error())
 	}
