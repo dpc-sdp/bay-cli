3a125b98c47825b8ee2702aebfcbb9dd6ce4429e
Guy Owen <guy.owen@dpc.vic.gov.au>
2025-01-13 16:43:45 +1100

    Refactored data structure to group by project.

--- cmd/elastic-cloud/delete-stale.go
+++ cmd/elastic-cloud/delete-stale.go
@@ -28,6 +28,8 @@ type IndexSettings struct {
 
 type Indices map[string]IndexSettings
 
+type IndicesByProject map[string][]Indices
+
 type AliasAttr struct {
 	IsHidden bool `json:"is_hidden"`
 }
@@ -60,46 +62,57 @@ func DeleteStaleIndices(c *cli.Context) error {
 	}
 
 	indicesList := Indices{}
+	IndicesByProject := IndicesByProject{}
 
 	if err := json.NewDecoder(settings.Body).Decode(&indicesList); err != nil {
 		return errors.Wrap(err, "Error parsing the response body")
 	} else {
 		for k, i := range indicesList {
 			if strings.Contains(k, "elasticsearch_index") {
-				a, err := esapi.IndicesGetAliasRequest{Index: []string{k}}.Do(context.TODO(), client)
+				// TODO: Add handling of non hash based Drupal indices.
+				hash := strings.Split(k, "--")[0]
+				project, err := hashes.LookupProjectFromHash(hash)
 				if err != nil {
-					return err
-				}
-				aliasList := map[string]Aliases{}
-				if err := json.NewDecoder(a.Body).Decode(&aliasList); err != nil {
-					return errors.Wrap(err, "Error parsing the response body")
-				}
-				now := time.Now().UnixMilli()
-				created, err := strconv.ParseInt(i.IndexItem.IndexDetail.CreationDate, 10, 64)
-				if err != nil {
-					return err
+					fmt.Printf("Error looking up project for hash %+v\n", hash)
 				}
 
-				diffInDays := (now - created) / (1000 * 60 * 60 * 24)
-
-				if diffInDays > age {
-					// Add helper function to compute hash from k.
-					hash := strings.Split(k, "--")[0]
-					project, err := hashes.LookupProjectFromHash(hash)
+				IndicesByProject[project] = append(IndicesByProject[project], map[string]IndexSettings{k: i})
+			}
+		}
+		for p, i := range IndicesByProject {
+			fmt.Printf("Scanning %s indices: \n", p)
+			for _, j := range i {
+				for k, i := range j {
+					// TODO: Add a parameter to exclude a project by it's name.
+					a, err := esapi.IndicesGetAliasRequest{Index: []string{k}}.Do(context.TODO(), client)
 					if err != nil {
-						fmt.Printf("Error looking up project for hash %+v\n", hash)
+						return err
+					}
+					aliasList := map[string]Aliases{}
+					if err := json.NewDecoder(a.Body).Decode(&aliasList); err != nil {
+						return errors.Wrap(err, "Error parsing the response body")
+					}
+					now := time.Now().UnixMilli()
+					created, err := strconv.ParseInt(i.IndexItem.IndexDetail.CreationDate, 10, 64)
+					if err != nil {
+						return err
 					}
 
-					fmt.Printf("Project: %+v\n", project)
+					diffInDays := (now - created) / (1000 * 60 * 60 * 24)
 
-					if len(aliasList[k].Aliases) > 0 {
-						for aliasName := range aliasList[k].Aliases {
-							fmt.Fprintf(c.App.Writer, "The index %s is %d days old but will not be deleted because it has an associated alias %s\n", k, diffInDays, aliasName)
+					if diffInDays > age {
+						// Add helper function to compute hash from k.
+
+						if len(aliasList[k].Aliases) > 0 {
+							for aliasName := range aliasList[k].Aliases {
+								fmt.Fprintf(c.App.Writer, "The index %s is %d days old but will not be deleted because it has an associated alias %s\n", k, diffInDays, aliasName)
+							}
+						} else {
+							fmt.Fprintf(c.App.Writer, "The index %s is %d days old and will be marked for deletion\n", k, diffInDays)
+							deleteList = append(deleteList, k)
 						}
-					} else {
-						fmt.Fprintf(c.App.Writer, "The index %s is %d days old and will be marked for deletion\n", k, diffInDays)
-						deleteList = append(deleteList, k)
 					}
+
 				}
 			}
 		}
